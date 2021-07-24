2ff7b68500a7d8118adce06d3cae13fa35cc3633
hovaroyan <hovaroyan@gmail.com>
2021-07-25 01:05:52 +0400

    added banner to home

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedAboutComponent } from './shared-about/shared-about.component';
+import { SharedBannerComponent } from './shared-banner/shared-banner.component';
 
 
 
 @NgModule({
   declarations: [
-    SharedAboutComponent
+    SharedAboutComponent,
+    SharedBannerComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    SharedAboutComponent
+    SharedAboutComponent,
+    SharedBannerComponent
   ]
 })
 export class SharedModule { }
